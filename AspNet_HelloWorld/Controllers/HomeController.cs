13c264a77633fa01355d68f71978e3e214bb1386
Peter Jausovec <peterj@microsoft.com>
2015-10-27 22:55:51 -0700

    Adding sample asp.net project

---
+++ AspNet_HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace MvcSample.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.OS =  "OS: " + Environment.GetEnvironmentVariable("OS");
+            return View();
+        }
+    }
+}
